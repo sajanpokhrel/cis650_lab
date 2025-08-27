93a7638b19fe680b1329b86f4a02edcfff0fafe2
Sajan Pokhrel <spokhrel1@murraystate.edu>
2025-08-27 11:04:24 -0500

    done with task 2

---
+++ Lab1-Statements/task2.py
@@ -0,0 +1,10 @@
+quantity = int(input('Enter number of bags'))
+price = float(input('Enter price per bag'))
+print('total cost: ' , quantity * price) 
+
+
+
+miles_driven = int(input('Enter miles driven:'))
+gallons_used = float(input('Enter gallons used:'))
+miles_per_gallon = miles_driven / gallons_used 
+print(f'Miles per gallon is {miles_per_gallon:4.2f}') 
---
+++ Lab1-Statements/task3.py
@@ -0,0 +1,5 @@
+name = input('what is your name?')
+if len(name) > 0:
+     print('Hello', name)
+else :
+     print('Enter your name, please!') 
